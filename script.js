9647d750d623e3fe445c3dd9ac37e925da732bd1
Popa Cristian Alexandru <81997541+EnlightenedTop@users.noreply.github.com>
2022-06-06 16:10:33 +0200

    Add files via upload
    already changed a few things , probably gonna add 10000000 repositories today idk xd

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const eye = document.querySelector(".eye-ball");
 const content = document.querySelector(".content--section");
-const title = document.querySelector(".title1");
-const title2 = document.querySelector(".title2");
-const title3 = document.querySelector(".title3");
-const title4 = document.querySelector(".title4");
+const titles = document.querySelectorAll(".title");
+
 const wholeEye = document.querySelector(".eye");
 const eyeBall = document.querySelector(".eye-moving");
 eye.style.transform = `rotate(${225}deg)`;
@@ -36,15 +34,15 @@ document.documentElement.addEventListener("mousemove", () => {
 //   eye.style.transform = `rotate(${50}deg)`;
 // });
 
-const eyeTransfrom = function () {
-  eye.style.color = "green";
-};
-
-title.addEventListener("mouseover", function () {
-  // wholeEye.classList.toggle("eye--big");
-  eyeBall.classList.toggle("eye-ball--big");
-});
-title.addEventListener("mouseleave", function () {
-  // wholeEye.classList.toggle("eye--big");
-  eyeBall.classList.toggle("eye-ball--big");
-});
+titles.forEach((title) =>
+  title.addEventListener("mouseover", function () {
+    // wholeEye.classList.toggle("eye--big");
+    eyeBall.classList.toggle("eye-ball--big");
+  })
+);
+titles.forEach((title) =>
+  title.addEventListener("mouseleave", function () {
+    // wholeEye.classList.toggle("eye--big");
+    eyeBall.classList.toggle("eye-ball--big");
+  })
+);
